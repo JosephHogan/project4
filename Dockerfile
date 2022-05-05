86bec78de48c0f3b1ac26bdb23214d76d13db662
JosephHogan <88689431+JosephHogan@users.noreply.github.com>
2022-05-04 18:57:56 -0700

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:latest
 WORKDIR /project
 COPY pythonCode1.py /project
 COPY requirements.txt /project
+RUN apt-get update && apt-get install -y python3-opencv
 RUN pip3 install -r requirements.txt
 CMD ["python3","pythonCode1.py"]
 
--- pythonCode1.py
+++ pythonCode1.py
@@ -8,6 +8,7 @@ from socket import gethostname
 from pprint import pprint
 from time import gmtime, mktime
 from os.path import exists, join
+import os
 
 blackblankimage = random.randint(0, 255) * np.ones(shape=[512, 512, 3], dtype=np.uint8)
 
@@ -23,10 +24,20 @@ def create_self_signed_cert():
     k=crypto.PKey()
     k.generate_key(crypto.TYPE_RSA, 2048)
     cert=crypto.X509()
-    cert.get_subject().C="United States"
+    cert.get_subject().C="US"
     cert.get_subject().ST="NV"
     cert.get_subject().L="Reno"
     cert.get_subject().O="University of Nevada, Reno"
     cert.get_subject().OU="CSE"
     cert.get_subject().CN=gethostname()
     cert.set_serial_number(42)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(157680000)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha512')
+    open("josephhogan_selfSignedCertificate.crt","wb").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    open("josephhogan_privateKey.PEM","wb").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+    cmd='openssl rsa -in josephhogan_privateKey.PEM -pubout -out josephhogan_publicKey.PEM'
+    os.system(cmd)
+create_self_signed_cert()
